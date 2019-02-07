63d9891b994fe396e7eab372baf9a7b5f8e9f4df
Tomasz Furca <furcatomasz@gmail.com>
2019-02-07 23:06:06 +0100

    fixed scaling, added backspace manager, singleTop app

--- app/src/main/java/com/example/slavsgame/AppWebViewClient.java
+++ app/src/main/java/com/example/slavsgame/AppWebViewClient.java
@@ -9,12 +9,6 @@ public class AppWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if(Uri.parse(url).getHost().endsWith("slavsgame.com")) {
-            return false;
-        }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        view.getContext().startActivity(intent);
-        return true;
+       return false;
     }
 }
--- app/src/main/java/com/example/slavsgame/WebViewActivity.java
+++ app/src/main/java/com/example/slavsgame/WebViewActivity.java
@@ -2,26 +2,37 @@ package com.example.slavsgame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import org.xwalk.core.XWalkView;
 
 public class WebViewActivity extends Activity {
 
     private WebView mWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.loadUrl("http://slavsgame.com");
         mWebView.setWebViewClient(new AppWebViewClient());
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
     }
 
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 }
